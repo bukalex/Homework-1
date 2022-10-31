6080ec608fdbf01446258db68cdc2ab3996ff0c3
gurylevalexey <gurylevalexey@gmail.com>
2022-11-01 02:18:42 +0300

    Задание выполнено.

--- src/Main.java
+++ src/Main.java
@@ -49,5 +49,32 @@ public class Main {
         System.out.println(frog);
         frog += 4;
         System.out.println(frog);
+
+        System.out.println(" ");
+
+        var boxer1Weight = 78.2;
+        var boxer2Weight = 82.7;
+        var sum = boxer2Weight + boxer1Weight;
+        var dif = boxer2Weight - boxer1Weight;
+        System.out.println("Задание 6:");
+        System.out.println(sum);
+        System.out.println(dif);
+
+        System.out.println(" ");
+
+        System.out.println("Задание 7:");
+        dif = boxer2Weight % boxer1Weight;
+        System.out.println(dif);
+
+        System.out.println(" ");
+
+        var totalTime = 640;
+        var personalTime = 8;
+        var workersNum = totalTime/personalTime;
+        System.out.println("Задание 8:");
+        System.out.println("Всего в компании работает " + workersNum + " человек.");
+        workersNum += 94;
+        totalTime = workersNum * personalTime;
+        System.out.println("Если в компании работает " + workersNum + " человек, то между ними может быть поделено " + totalTime + " часов работы.");
     }
 }
