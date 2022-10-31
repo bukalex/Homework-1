c3bf4f9ff21c785418a001c55fcd5a3b353a910c
gurylevalexey <gurylevalexey@gmail.com>
2022-11-01 01:30:11 +0300

    Задание выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Задание 1:");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println(" ");
+
+        dog += 4;
+        cat += 4;
+        paper += 4;
+        System.out.println("Задание 2:");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println(" ");
+
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+        System.out.println("Задание 3:");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
